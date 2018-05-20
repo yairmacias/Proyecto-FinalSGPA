af0826e0bec3aaaec393ed4a41888e47f2f1a4c0
jcontreras97 <36319122+jcontreras97@users.noreply.github.com>
2018-05-20 02:09:11 -0500

    Add files via upload

---
+++ SistemaGestorParaAcademias/src/mx/SGPA/domain/Planeacion.java
@@ -0,0 +1,76 @@
+
+package mx.SGPA.domain;
+
+/**
+ *
+ * @author Jasiel
+ */
+public class Planeacion {
+    
+    int unidad;
+    String temas;
+    int fechas;
+    String tareasPracticas;
+    String tecnicaDidactica;
+    int idPlanCurso;
+    
+    public Planeacion(int unidad, String temas, int fechas, String tareasPracticas, String tecnicasDidacticas, int idPlanCurso ){
+        
+        this.unidad = unidad;
+        this.temas = temas;
+        this.fechas = fechas;
+        this.tareasPracticas = tareasPracticas;
+        this.tecnicaDidactica = tecnicasDidacticas;
+        this.idPlanCurso = idPlanCurso;
+    }
+
+    public int getUnidad() {
+        return unidad;
+    }
+
+    public void setUnidad(int unidad) {
+        this.unidad = unidad;
+    }
+
+    public String getTemas() {
+        return temas;
+    }
+
+    public void setTemas(String temas) {
+        this.temas = temas;
+    }
+
+    public int getFechas() {
+        return fechas;
+    }
+
+    public void setFechas(int fechas) {
+        this.fechas = fechas;
+    }
+
+    public String getTareasPracticas() {
+        return tareasPracticas;
+    }
+
+    public void setTareasPracticas(String tareasPracticas) {
+        this.tareasPracticas = tareasPracticas;
+    }
+
+    public String getTecnicaDidactica() {
+        return tecnicaDidactica;
+    }
+
+    public void setTecnicaDidactica(String tecnicaDidactica) {
+        this.tecnicaDidactica = tecnicaDidactica;
+    }
+
+    public int getIdPlanCurso() {
+        return idPlanCurso;
+    }
+
+    public void setIdPlanCurso(int idPlanCurso) {
+        this.idPlanCurso = idPlanCurso;
+    }
+    
+    
+}
---
+++ SistemaGestorParaAcademias/src/mx/SGPA/domain/TemasTratar.java
@@ -0,0 +1,36 @@
+
+package mx.SGPA.domain;
+
+/**
+ *
+ * @author Jasiel
+ */
+public class TemasTratar {
+   
+    String temasTratar;
+    int idMinuta;
+    
+    public TemasTratar(String temasTratar, int idMinuta){
+        
+        this.temasTratar = temasTratar;
+        this.idMinuta = idMinuta;
+    }
+
+    public String getTemasTratar() {
+        return temasTratar;
+    }
+
+    public void setTemasTratar(String temasTratar) {
+        this.temasTratar = temasTratar;
+    }
+
+    public int getIdMinuta() {
+        return idMinuta;
+    }
+
+    public void setIdMinuta(int idMinuta) {
+        this.idMinuta = idMinuta;
+    }
+    
+    
+}
